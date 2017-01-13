0c6c2795737b6ba7458cf1436dec2f5e895b4da3
johnny18445 <jyoti.johney@gmail.com>
2017-01-13 22:07:51 +0530

    Custom Functional interface implementation

--- src/main/java/com/java8/functionalinterface/MessageFunctionalInterface.java
+++ src/main/java/com/java8/functionalinterface/MessageFunctionalInterface.java
@@ -4,17 +4,19 @@ import java.time.LocalDate;
 import java.util.Locale;
 
 /**
+ * FunctionalInterface implementation
+ *
  * Created by jyotipoddar on 13-01-2017.
  */
 @FunctionalInterface
 interface MessageFunctionalInterface {
 
-    public String getWelcomeMessage(String msg);  // abstract method in interface
+    String getWelcomeMessage(String msg);  // abstract method in interface
 
-    public default String getCurrentTime() {  // default method with definition in interface
+    default String getCurrentTime() {  // default method with definition in interface
         return LocalDate.now().toString();
     }
-    public static String getLocaleInfo() {  // static method definition in interface
+    static String getLocaleInfo() {  // static method definition in interface
         return Locale.getDefault().toString();
     }
 }
